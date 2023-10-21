f89c44b498bbfb39e1935db6e7d756737faf8355
Charlie <chrlie.w@proton.me>
2023-10-21 12:49:06 +0100

    Skeleton

---
+++ src/load-page.js
@@ -0,0 +1,35 @@
+
+export function loadPage(){
+    // render content
+    const content = document.getElementById('content');
+
+    // create header 
+    const header = document.createElement('header');
+    header.classList.add('header');
+    content.appendChild(header);
+
+    const headerText = document.createElement('h1');
+    headerText.textContent = 'Todo List';
+    header.appendChild(headerText);
+
+    // create main
+    const main = document.createElement('main');
+    main.classList.add('main');
+    content.appendChild(main);
+
+    // create sidebar
+    const sidebar = document.createElement('div');
+    sidebar.classList.add('sidebar');
+    main.appendChild(sidebar);
+
+    // create main content
+    const mainContent = document.createElement('div');
+    mainContent.classList.add('main-content');
+    main.appendChild(mainContent);
+
+    // create footer
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+    content.appendChild(footer);
+    
+}
