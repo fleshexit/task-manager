29304d97d7ed2215e1ae7fcab4b68ebab91fe298
Charlie <chrlie.w@proton.me>
2023-10-21 12:58:30 +0100

    Load page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("test")
+import { loadPage } from "./load-page";
+
+function clearContent(){
+    const content = document.getElementById('content');
+    while(content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+}
+
+loadPage();
